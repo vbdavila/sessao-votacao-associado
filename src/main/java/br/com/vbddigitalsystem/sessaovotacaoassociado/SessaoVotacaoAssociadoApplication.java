fd973c593a7c24fe2114dee30d2e14074ba98d6a
Vanderlei Brondani Davila <vbdavila@macbook-pro-vbdavila.local>
2021-10-04 13:55:39 -0300

    Initial commit

---
+++ src/main/java/br/com/vbddigitalsystem/sessaovotacaoassociado/SessaoVotacaoAssociadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vbddigitalsystem.sessaovotacaoassociado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoVotacaoAssociadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaoVotacaoAssociadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vbddigitalsystem/sessaovotacaoassociado/SessaoVotacaoAssociadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vbddigitalsystem.sessaovotacaoassociado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessaoVotacaoAssociadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
